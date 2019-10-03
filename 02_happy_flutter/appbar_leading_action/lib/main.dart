f95bb83079ca20c964e081a8fa321482c2022280
andiankas <rustiantoandi17@gmail.com>
2019-10-03 23:56:41 +0700

    learn appbar

---
+++ 02_happy_flutter/appbar_leading_action/lib/main.dart
@@ -0,0 +1,44 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(new MaterialApp(home: new Application()));
+
+class Application extends StatefulWidget {
+  @override
+  _ApplicationState createState() => _ApplicationState();
+}
+
+class _ApplicationState extends State<Application> {
+
+  String txt = "";
+
+  @override
+  Widget build(BuildContext context) {
+    return new Scaffold(
+      appBar: new AppBar(
+        backgroundColor: Colors.orange,
+        title: new Text("AppBar Widget"),
+        leading: new Icon(Icons.menu),
+        actions: <Widget>[
+          new IconButton(
+            icon: new Icon(Icons.access_time),
+            onPressed: (){
+              txt = "This is arrow button 1";
+            },
+          ),
+          new IconButton(
+            icon: new Icon(Icons.data_usage),
+            onPressed: (){
+              txt = "This is arrow button 2";
+            },
+          ),
+        ],
+      ),
+      body: new Container(
+        child: new Center(
+          child: new Text(txt),
+        ),
+      )
+    );
+  }
+}
+
