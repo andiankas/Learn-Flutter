131a9883555e62484055645356b504a83a25747a
andiankas <rustiantoandi17@gmail.com>
2019-10-08 21:29:13 +0700

    get data from db

--- 00_flutter_basic/crud_phpmysql/lib/main.dart
+++ 00_flutter_basic/crud_phpmysql/lib/main.dart
@@ -1,4 +1,7 @@
+import 'dart:convert';
 import 'package:flutter/material.dart';
+import 'dart:async';
+import 'package:http/http.dart' as http;
 
 void main() => runApp(MaterialApp(title: "My Store",home: new Home(),));
 
@@ -8,12 +11,40 @@ class Home extends StatefulWidget {
 }
 
 class _HomeState extends State<Home> {
+  
+  Future<List> getData() async{
+    final response = await http.get("http://192.168.1.146/xdev/xlearn/flutter_mystore/getdata.php");
+    return json.decode(response.body);
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text("My Store"),
+      ),body: new FutureBuilder<List>(
+        future: getData(),
+        builder: (context,snapshot){
+          if (snapshot.hasError) print(snapshot.error);
+          return snapshot.hasData ? new ItemList(list: snapshot.data) : new Center(child: new CircularProgressIndicator(),);
+        },
       ),
+    );
+  }
+}
+
+class ItemList extends StatelessWidget {
+
+  final List list;
+  ItemList({this.list});
+
+  @override
+  Widget build(BuildContext context) {
+    return ListView.builder(
+      itemCount: list==null ? 0 : list.length,
+      itemBuilder: (context,i){
+        return new Text(list[i]["item_name"]);
+      },
     );
   }
 }
