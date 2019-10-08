acad1e84caf4e7d078adb24131d1d3b24e335151
andiankas <rustiantoandi17@gmail.com>
2019-10-08 23:56:08 +0700

    get data from db and show detail data

---
+++ 00_flutter_basic/crud_phpmysql/lib/detail.dart
@@ -0,0 +1,64 @@
+import 'package:flutter/material.dart';
+
+
+class Detail extends StatefulWidget {
+
+  final List list;
+  final int index;
+  Detail({this.index, this.list});
+
+  @override
+  _DetailState createState() => _DetailState();
+}
+
+class _DetailState extends State<Detail> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: new Text("${widget.list[widget.index]['item_name']}"),
+      ),
+      floatingActionButton: new FloatingActionButton(
+        child: new Icon(Icons.add),
+        onPressed: (){
+          
+        },
+      ),
+      body: new Container(
+        height: 250.0,
+        padding: const EdgeInsets.all(20.0),
+        child: new Card(
+          child: new Center(
+            child: new Column(
+              children: <Widget>[
+                new Padding(padding: const EdgeInsets.only(top: 30.0),),
+                new Text(widget.list[widget.index]['item_name'], style: new TextStyle(fontSize: 20.0),),
+                new Text("Kode : ${widget.list[widget.index]['item_code']}", style: new TextStyle(fontSize: 18.0),),
+                new Text("Harga : ${widget.list[widget.index]['price']}", style: new TextStyle(fontSize: 18.0),),
+                new Text("Stok : ${widget.list[widget.index]['stock']}", style: new TextStyle(fontSize: 18.0),),
+                new Padding(padding: const EdgeInsets.only(top: 30.0),),
+
+                new Row(
+                  mainAxisSize: MainAxisSize.min,
+                  children: <Widget>[
+                    new RaisedButton(
+                      child: new Text("Edit"),
+                      color: Colors.green,
+                      onPressed: (){},
+                    ),
+                    Padding(padding: const EdgeInsets.all(10.0),),
+                    new RaisedButton(
+                      child: new Text("Delete"),
+                      color: Colors.red,
+                      onPressed: (){},
+                    ),
+                  ],
+                ),
+              ],
+            )
+          ),
+        ),
+      ),
+    );
+  }
+}
--- 00_flutter_basic/crud_phpmysql/lib/main.dart
+++ 00_flutter_basic/crud_phpmysql/lib/main.dart
@@ -2,8 +2,12 @@ import 'dart:convert';
 import 'package:flutter/material.dart';
 import 'dart:async';
 import 'package:http/http.dart' as http;
+import 'package:crud_phpmysql/detail.dart';
 
-void main() => runApp(MaterialApp(title: "My Store",home: new Home(),));
+void main() => runApp(MaterialApp(
+      title: "My Store",
+      home: new Home(),
+    ));
 
 class Home extends StatefulWidget {
   @override
@@ -11,9 +15,9 @@ class Home extends StatefulWidget {
 }
 
 class _HomeState extends State<Home> {
-  
-  Future<List> getData() async{
-    final response = await http.get("http://192.168.1.146/xdev/xlearn/flutter_mystore/getdata.php");
+  Future<List> getData() async {
+    final response = await http
+        .get("http://192.168.43.35/xdev/xlearn/flutter_mystore/getdata.php");
     return json.decode(response.body);
   }
 
@@ -22,11 +26,16 @@ class _HomeState extends State<Home> {
     return Scaffold(
       appBar: AppBar(
         title: Text("My Store"),
-      ),body: new FutureBuilder<List>(
+      ),
+      body: new FutureBuilder<List>(
         future: getData(),
-        builder: (context,snapshot){
+        builder: (context, snapshot) {
           if (snapshot.hasError) print(snapshot.error);
-          return snapshot.hasData ? new ItemList(list: snapshot.data) : new Center(child: new CircularProgressIndicator(),);
+          return snapshot.hasData
+              ? new ItemList(list: snapshot.data)
+              : new Center(
+                  child: new CircularProgressIndicator(),
+                );
         },
       ),
     );
@@ -34,16 +43,30 @@ class _HomeState extends State<Home> {
 }
 
 class ItemList extends StatelessWidget {
-
   final List list;
   ItemList({this.list});
-
   @override
   Widget build(BuildContext context) {
     return ListView.builder(
-      itemCount: list==null ? 0 : list.length,
-      itemBuilder: (context,i){
-        return new Text(list[i]["item_name"]);
+      itemCount: list == null ? 0 : list.length,
+      itemBuilder: (context, i) {
+        return Container(
+          padding: const EdgeInsets.all(5.0),
+          child: GestureDetector(
+            onTap: () => Navigator.of(context).push(
+              new MaterialPageRoute(
+                builder: (BuildContext context) => new Detail(list: list,index: i,)
+              ),
+            ),
+            child: Card(
+              child: new ListTile(
+                title: new Text(list[i]['item_name']),
+                leading: new Icon(Icons.widgets),
+                subtitle: new Text("Stock : ${list[i]['stock']}"),
+              ),
+            ),
+          ),
+        );
       },
     );
   }
