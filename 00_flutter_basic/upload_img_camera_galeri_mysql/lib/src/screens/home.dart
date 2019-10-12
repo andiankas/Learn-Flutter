4a67aea82cde37b3aabe78a19a215c54b0041613
andiankas <rustiantoandi17@gmail.com>
2019-10-13 01:27:31 +0700

    bug move folder path

--- 00_flutter_basic/upload_img_camera_galeri_mysql/lib/src/screens/home.dart
+++ 00_flutter_basic/upload_img_camera_galeri_mysql/lib/src/screens/home.dart
@@ -1,5 +1,10 @@
 import 'dart:io';
 import 'package:flutter/material.dart';
+import 'package:image_picker/image_picker.dart';
+import 'dart:async';
+import 'package:async/async.dart';
+import 'package:http/http.dart' as http;
+import 'package:path/path.dart';
 
 class Home extends StatefulWidget {
   @override
@@ -10,16 +15,80 @@ class _HomeState extends State<Home> {
 
   File _image;
 
+  Future getImageGallery() async  {
+    var imageFile  = await ImagePicker.pickImage(source: ImageSource.gallery);
+    
+    setState(() {
+      _image = imageFile;
+    });
+  }
+
+  Future getImageCamera() async  {
+    var imageFile = await ImagePicker.pickImage(source: ImageSource.camera);
+    setState(() {
+      _image = imageFile;
+    });
+  }
+
+  Future upload(File imageFile) async  {
+    //  menangkap gambarnya
+    var stream = new http.ByteStream(DelegatingStream.typed(imageFile.openRead()));
+    var length = await imageFile.length();
+    var uri = Uri.parse("http://192.168.43.35/xdev/xlearn/flutter_img_camera_gallery/upload.php");
+
+    var request = new http.MultipartRequest("POST", uri);
+
+    var multipartFile = new http.MultipartFile("image", stream, length, filename: basename(imageFile.path));
+
+    request.files.add(multipartFile);
+
+    var response = await request.send();
+
+    if (response.statusCode == 200) {
+      print("img upload");
+    }else{
+      print("upload failed");
+    }
+  }
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      
       appBar: AppBar(
         title: Text("Upload Image"),
         backgroundColor: Colors.grey[850],
       ),
-      body: Column(
+      body: 
+      Column(
         children: <Widget>[
-
+          Center(
+            // cek, if img null, maka tampilkan text 'no img selected'. else, inisiasi img file ke vafiable _image
+            child: _image == null 
+            ? new Text("No Img Selected") 
+            : new Image.file( _image ), 
+          ),
+         
+          Row(
+            children: <Widget>[
+              RaisedButton(
+                child: Icon(Icons.image),
+                onPressed: getImageGallery,
+              ),
+              Padding(padding: const EdgeInsets.all(10.0),),
+              RaisedButton(
+                child: Icon(Icons.camera_alt),
+                onPressed: getImageCamera,
+              ),
+              Padding(padding: const EdgeInsets.all(10.0),),
+              Expanded(child: Container() ),
+              RaisedButton(
+                child: Text("Upload"),
+                onPressed: (){
+                  upload( _image );
+                },
+              ),
+            ],
+          ),
         ],
       ),
     );
