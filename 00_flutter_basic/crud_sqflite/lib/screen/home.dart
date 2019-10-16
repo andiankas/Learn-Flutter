1e926566a764bc65ecd1573a99ee5af43610ce9a
andiankas <rustiantoandi17@gmail.com>
2019-10-17 06:31:13 +0700

    view home

--- 00_flutter_basic/crud_sqflite/lib/screen/home.dart
+++ 00_flutter_basic/crud_sqflite/lib/screen/home.dart
@@ -10,11 +10,23 @@ class _HomeState extends State<Home> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      floatingActionButton: FloatingActionButton(
+        child: Icon(
+          Icons.add,
+          color: Colors.white
+        ),
+        backgroundColor: Colors.black,
+        onPressed: (){},
+      ),
        appBar: AppBar(
-         leading: Image.asset("img/logo.png"),
+         leading: Container(
+           padding: EdgeInsets.all(10),
+           child: Image.asset("img/logo.jpg"),
+         ),
          title: Text("Simple Note", style: TextStyle(color: Colors.white, fontSize: 25, fontWeight: FontWeight.w300,),),
          backgroundColor: Colors.black,
        ),
+       backgroundColor: Colors.grey[300],
        body: Container(),
     );
   }
