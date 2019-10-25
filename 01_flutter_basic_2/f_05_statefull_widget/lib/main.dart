7150e44e5c8f3235105f13e5ba048a5817d66229
andiankas <rustiantoandi17@gmail.com>
2019-10-26 01:58:30 +0700

    statefull widget in case with number increment

---
+++ 01_flutter_basic_2/f_05_statefull_widget/lib/main.dart
@@ -0,0 +1,47 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatefulWidget {
+  MyApp({Key key}) : super(key: key);
+
+  @override
+  _MyAppState createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+
+  int number = 1;
+
+  void tambahData(){
+    setState(() {
+      number = number+1;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text("Statefull Widget"),
+        ),
+        body: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              Text(
+                number.toString(), // mengubah number int menjadi string
+                style: TextStyle(fontSize: 20 * number.toDouble()),
+              ),
+              RaisedButton(
+                child: Text("Tambah"),
+                onPressed: tambahData,
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
