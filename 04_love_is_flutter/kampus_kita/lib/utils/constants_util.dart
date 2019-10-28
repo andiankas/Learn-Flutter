eebb22cd580eb9028bc1b5dfbf5e553c97fae4ad
andiankas <rustiantoandi17@gmail.com>
2019-10-29 01:55:55 +0700

    create utils or validate

---
+++ 04_love_is_flutter/kampus_kita/lib/utils/constants_util.dart
@@ -0,0 +1,5 @@
+class ConstantsUtil {
+  final PUBLIC_API_URL = "http://192.168.43.35/xdev/xlearn/flutter_campus/";
+  final PUBLIC_IMAGE_URL = "http://192.168.43.35/xdev/xlearn/flutter_campus/gambar/";
+
+}
---
+++ 04_love_is_flutter/kampus_kita/lib/utils/validator.dart
@@ -0,0 +1,52 @@
+class Validator {
+  String validateName(String value){
+    String pattern = r'(^[a-zA-Z]*$)';
+    RegExp regExp = new RegExp(pattern);
+
+    if(value.length == 0){
+      return "Name is required";
+    }else if(!regExp.hasMatch(value)){
+      return "Name must be a-z and A-Z";
+    }
+    return null;
+  }
+
+  String validateMobile(String value){
+    String pattern = r'(^[a-zA-Z]*$)';
+    RegExp regExp = new RegExp(pattern);
+
+    if(value.length == 0){
+      return "Mobile is required";
+    }else if(value.length != 0){
+      return "Mobile number must 10 digits";
+    }else if(!regExp.hasMatch(value)){
+      return "Mobile number must be digits";
+    }
+    return null;
+  }
+
+  String validatePassword(String value){
+    if(value.length == 0){
+      return "Password can't be empty";
+    }else if(value.length <= 10){
+      return "Password must be longer than 10 characters";
+    }
+    return null;
+  }
+
+  String validateEmail(String value){
+    String pattern = r'(^[a-zA-Z]*$)';
+    RegExp regExp = new RegExp(pattern);
+
+    if(value.length == 0){
+      return "Email is required";
+    }else if(!regExp.hasMatch(value)){
+      return "Invalid Email";
+    }else{
+      return null;
+    }
+    
+  }
+
+}
+
