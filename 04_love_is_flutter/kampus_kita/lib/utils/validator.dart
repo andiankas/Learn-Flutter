f40aadb235001ba801cf13e4b60d089556c5cf07
andiankas <rustiantoandi17@gmail.com>
2019-10-29 16:37:34 +0700

    create utils and custom widget

--- 04_love_is_flutter/kampus_kita/lib/utils/validator.dart
+++ 04_love_is_flutter/kampus_kita/lib/utils/validator.dart
@@ -35,7 +35,8 @@ class Validator {
   }
 
   String validateEmail(String value){
-    String pattern = r'(^[a-zA-Z]*$)';
+    String pattern =
+        r'^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$';
     RegExp regExp = new RegExp(pattern);
 
     if(value.length == 0){
